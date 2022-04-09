c6e4a50cec9fbe2ac4935a7c891b4790f372d587
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-04-09 20:30:47 +0530

    feat(io): get midi-output working

--- src/svelte/nodes/midi-output.svelte
+++ src/svelte/nodes/midi-output.svelte
@@ -1,23 +1,64 @@
-<script lang="ts">
-    import { init } from "../../ts/midi-io";
-    import { storage } from "../../ts/storage";
-    import { onMount } from "svelte";
+<script lang="ts" context="module">
+    let activeOutput: Output;
+    var runningStatus;
 
-    const ports = { outputs: [] };
-    let activeOutput;
+    export const outputs = {};
+    export const inputs = {
+        MIDI: {
+            call: (status, data1, data2) => {
+                const message = [status? status: runningStatus];
+                if (typeof data1 === "number") message.push(data1);
+                if (typeof data1 === "number" && typeof data2 === "number") message.push(data2);
+                activeOutput.send(message);
+                runningStatus = status? status: runningStatus;
+            },
+        },
+    };
+</script>
+
+<script lang="ts">
+    import { storage } from "../../ts/storage";
+    import { onMount, tick, createEventDispatcher } from "svelte";
+    import { Output, WebMidi } from "webmidi";
+
+    const dispatch = createEventDispatcher();
+
+    let outputNames: Array<string> = [];
+    let activeOutputName: string;
 
     onMount(async () => {
-        const { outputs } = await init();
-        activeOutput = await outputStore.getValue();
-        ports.outputs = outputs;
+        await WebMidi.enable();
+        WebMidi.addListener("portschanged", updateOutputs);
+        updateOutputs();
     });
+
+    async function updateOutputs() {
+        const newOutputNames = [];
+        const outputs = WebMidi.outputs;
+        for (const output of outputs) {
+            newOutputNames.push(output.name);
+        }
+        outputNames = newOutputNames;
+        const lastOutput = await outputStore.getValue();
+        if (outputNames.includes(lastOutput)) {
+            changeOutput(lastOutput);
+        }
+        await tick();
+        dispatch("resize", {});
+    }
 
     const outputStore = new storage.local("output");
 
     function onOutputChange(e: Event) {
         const selection = e.target as HTMLSelectElement;
         blurTarget(selection);
+        changeOutput(selection.value);
         outputStore.setValue(selection.value);
+    }
+
+    function changeOutput(name: string) {
+        activeOutputName = name;
+        activeOutput = WebMidi.getOutputByName(name);
     }
 
     function blurTarget(target: HTMLSelectElement) {
@@ -25,21 +66,12 @@
     }
 </script>
 
-<script lang="ts" context="module">
-    export const outputs = {};
-    export const inputs = {
-        "MIDI": {
-            call: (status, data1, data2) => {}
-        }
-    }
-</script>
-
 <main>
     <div>
         <span>Output Device</span>
-        <select on:change={onOutputChange} bind:value={activeOutput}>
-            {#each ports.outputs as output}
-                <option>{output.name}</option>
+        <select on:change={onOutputChange} bind:value={activeOutputName}>
+            {#each outputNames as output}
+                <option>{output}</option>
             {/each}
         </select>
     </div>
