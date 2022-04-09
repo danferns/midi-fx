e4e98e79194b9d8f7b4bd4d3196cfeef1ac6f31f
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-04-09 19:03:55 +0530

    chore: delete old files

--- src/ts/editor/node.ts
+++
@@ -1,9 +0,0 @@
-export default class Node {
-    name: string;
-    
-    params: Map<string, Parameter> = new Map();
-    inputs = new Map<string, NodeInput>();
-    outputs = new Map<string, NodeOutput>();
-
-    
-}
