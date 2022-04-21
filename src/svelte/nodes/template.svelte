2e452f67a60b8e2941c4f1602f18565692affe6d
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-04-21 22:30:14 +0530

    chore: add template node

---
+++ src/svelte/nodes/template.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { createEmitter } from "../../ts/util/NodeUtil";
+    export let id: string;
+    export const inputs: NodeInputs = {};
+    export const outputs: NodeOutputs = {};
+    const emit = createEmitter(id, outputs);
+</script>
