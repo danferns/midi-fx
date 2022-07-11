7bf8c5e534712b7f0333ce32ae61153db95a403e
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-07-11 09:44:02 +0530

    chore: set up PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,30 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), VitePWA({})],
+    plugins: [
+        svelte(),
+        VitePWA({
+            includeAssets: ["favicon-512px.png", "favicon.svg", "favicon-192px.png"],
+            manifest: {
+                name: "MIDI FX",
+                short_name: "MIDIFX",
+                description: "Node Editor for Creative MIDI Effects",
+                theme_color: "#3df5b8",
+                icons: [
+                    {
+                        src: "favicon-512px.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "favicon-192px.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    }
+                ]
+            }
+        }),
+    ],
     resolve: {
         alias: [{ find: "src", replacement: resolve(__dirname, "src") }],
     },
