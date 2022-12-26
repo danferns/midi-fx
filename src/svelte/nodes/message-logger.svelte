ea204ec6f2a5dc5a755b8cfd7cdf009b5c5c3acf
dannywritescode <57069381+dannywritescode@users.noreply.github.com>
2022-12-26 13:31:46 +0530

    feat: copy midi message logs to clipboard

--- src/svelte/nodes/message-logger.svelte
+++ src/svelte/nodes/message-logger.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { createEmitter } from "src/ts/util/NodeUtil";
     import Button from "../widgets/Button.svelte";
+    import HorizontalLayout from "../widgets/HorizontalLayout.svelte";
     import NodeUi from "../widgets/NodeUI.svelte";
     import Table from "../widgets/Table.svelte";
     import Title from "../widgets/Title.svelte";
@@ -23,15 +24,23 @@
     const emit = createEmitter(id, outputs);
 
     let messageArray: [number, number, number][] = [];
+
+    function copyToClipboard() {
+        const text = messageArray.map((message) => message.join(",")).join("\n");
+        navigator.clipboard.writeText(text);
+    }
 </script>
 
 <NodeUi width={300}>
     <Title>Message Logs</Title>
-    <Button
-        on:click={() => {
-            messageArray = [];
-        }}>Clear All</Button
-    >
+    <HorizontalLayout>
+        <Button
+            on:click={() => {
+                messageArray = [];
+            }}>Clear All</Button
+        >
+        <Button on:click={copyToClipboard}>Copy</Button>
+    </HorizontalLayout>
 </NodeUi>
 <NodeUi>
     <Table tableData={messageArray} tableHeaders={["Status", "Data 1", "Data 2"]} />
